3fbbf304c33928897a010b0a9c5b71b2062de1a4
Muhammad Aliyi Muhammad <122678719+mame-ali@users.noreply.github.com>
2023-01-23 16:07:29 +0300

    Add files via upload

---
+++ fizzbuzz-challenge.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        newArr=[]
+        for i in range(1,n+1):
+            if(i%3==0 and i%5==0):
+                newArr.append("FizzBuzz")
+            elif(i%3==0):
+                newArr.append("Fizz")
+            elif(i%5==0):
+                newArr.append("Buzz")
+            else:
+                newArr.append(str(i))
+        return newArr
